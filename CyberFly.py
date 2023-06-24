4c905ee43bf65da092565c10165050c4d901b460
BenTheBunny <100760252+BenTheBunny@users.noreply.github.com>
2023-06-24 18:02:50 +0500

    Add files via upload

--- CyberFly.py
+++ CyberFly.py
@@ -1,25 +1,36 @@
 import os
-import re
-import tkinter as tk
 from pypdf import PdfReader
+import tkinter as tk
+
 
 window = tk.Tk()
-entry = tk.Entry(width=50,fg="black",bg='white')
-label = tk.Label(
-    text="Вставьте путь до пдф файла", foreground="black" , background="AntiqueWhite1", width=45,height=5)
-label.pack()
-entry.pack(anchor="nw", padx=6,pady=6)
+window.geometry("700x250")
 
+
+def get_data():
+    label.config(text= entry.get(), font= ('Helvetica 13')) #я пытался настроить ее так чтобы использовать в os.scandir
+
+
+
+entry = tk.Entry(window, width= 42)
+entry.place(relx= .5, rely= .5, anchor= 'center')
+
+
+label= tk.Label(window, text="", font=('Helvetica 13'))
+label.pack()
+
+
+tk.Button(window, text= "Click to Show", command= get_data).place(relx= .7, rely= .5, anchor='center')
+entry.pack()
 window.mainloop()
 
-
-example_dir = 'C:\\Python\\Nado\\pd'
-with os.scandir(example_dir) as files:
+#читает пдф файл
+with os.scandir(get_data()) as files:
     for file in files:
         reader = PdfReader(file.path)
         number_of_pages = len(reader.pages)
-        for i in range(number_of_pages):
+        for i in range(1): #number_of_pages ):
             page = reader.pages[i]
             text = page.extract_text()
-            print(text)
+            print(text) #заменить обработкой документа
             
