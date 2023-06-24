4d564fd5aba8b320dc33d54a72dc1490fd9af560
BenTheBunny <100760252+BenTheBunny@users.noreply.github.com>
2023-06-24 13:52:50 +0500

    Add files via upload

--- CyberFly.py
+++ CyberFly.py
@@ -1,6 +1,17 @@
 import os
 import re
+import tkinter as tk
 from pypdf import PdfReader
+
+window = tk.Tk()
+entry = tk.Entry(width=50,fg="black",bg='white')
+label = tk.Label(
+    text="Вставьте путь до пдф файла", foreground="black" , background="AntiqueWhite1", width=45,height=5)
+label.pack()
+entry.pack(anchor="nw", padx=6,pady=6)
+
+window.mainloop()
+
 
 example_dir = 'C:\\Python\\Nado\\pd'
 with os.scandir(example_dir) as files:
@@ -8,6 +19,7 @@ with os.scandir(example_dir) as files:
         reader = PdfReader(file.path)
         number_of_pages = len(reader.pages)
         for i in range(number_of_pages):
-            page = reader.pages[0]
+            page = reader.pages[i]
             text = page.extract_text()
             print(text)
+            
