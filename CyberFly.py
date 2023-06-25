cd9d873a309d0776e94349edd12eef1eb2356c69
BenTheBunny <100760252+BenTheBunny@users.noreply.github.com>
2023-06-25 08:03:06 +0500

    Add files via upload

--- CyberFly.py
+++ CyberFly.py
@@ -4,12 +4,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-example_dir = st.text_input('Введите путь до пдф')
-with os.scandir(example_dir) as files:
-    for file in files:
-        reader = PdfReader(file.path)
-        number_of_pages = len(reader.pages)
-        for i in range(number_of_pages): #number_of_pages ):
-            page = reader.pages[i]
-            text = page.extract_text()
-            st.write(text)
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    with os.scandir(uploaded_file) as files:
+        for file in files:
+            reader = PdfReader(file.path)
+            number_of_pages = len(reader.pages)
+            for i in range(number_of_pages): #number_of_pages ):
+                page = reader.pages[i]
+                text = page.extract_text()
+                st.write(text)
